d95aa05b3ed81ac84a887834e8a649d805843123
gaetanvan <61145812+gaetanvan@users.noreply.github.com>
2023-01-26 10:45:22 +0100

    update

--- codeEditor.js
+++ codeEditor.js
@@ -4,7 +4,9 @@ const js  = document.getElementById('js');
 const style = document.querySelector('style')
 const testJs = document.getElementById('testJs')
 const tester = document.getElementById('tester')
+const progress = document.getElementById('progress')
 let characterCount = 0;
+const characterLenght = 250;
 
 html.value = localStorage.getItem('html');
 css.value = localStorage.getItem('css');
@@ -13,24 +15,33 @@ js.value = localStorage.getItem('js');
 html.addEventListener("keyup", getHtml);
 css.addEventListener("keyup", getCss);
 js.addEventListener("keyup", getJs);
+html.addEventListener("mouseover", showGif);
+html.addEventListener("mouseout", hideGif);
+
 
 function getHtml() {
     tester.innerHTML = html.value;
     localStorage.setItem('html' , html.value);
     characterCount = html.value.length;
-    document.getElementById('character').innerText = characterCount+ '/250';
+    progress.innerText = characterCount+ '/' + characterLenght;
+    progress.style.width = characterCount*100/characterLenght + '%'
 }
 
 function getCss() {
-    style.innerText = css.value;
+    style.innerText = '#tester ' + css.value ;
     localStorage.setItem('css' , css.value);
 }
 
 function getJs() {
     testJs.innerText = js.value;
-    console.log(js.value);
     localStorage.setItem('js' , js.value);
 }
 function run(){
     new Function(js.value)();
+}
+function showGif() {
+    document.getElementById('gif').style.visibility = 'visible';
+}
+function hideGif() {
+    document.getElementById('gif').style.visibility = 'hidden';
 }
